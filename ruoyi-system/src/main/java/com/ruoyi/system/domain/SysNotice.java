1fb3ead76d7e1c51f0ed4657b5a1768c774bfe88
hancher <hanchers@outlook.com>
2026-04-28 14:39:50 +0800

    SysNotice mybtis_flex 改造

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/SysNotice.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/SysNotice.java
@@ -1,102 +1,55 @@
 package com.ruoyi.system.domain;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
+import com.mybatisflex.annotation.Id;
+import com.mybatisflex.annotation.KeyType;
+import com.mybatisflex.annotation.Table;
+import com.ruoyi.common.annotation.Excel;
+import com.ruoyi.common.annotation.Excel.ColumnType;
 import com.ruoyi.common.core.domain.BaseEntity;
 import com.ruoyi.common.xss.Xss;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+import java.io.Serial;
 
 /**
  * 通知公告表 sys_notice
- * 
+ *
  * @author ruoyi
  */
-public class SysNotice extends BaseEntity
-{
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@Data(staticConstructor = "create")
+@Accessors(chain = true)
+@Table(value = "sys_notice")
+public class SysNotice extends BaseEntity {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     /** 公告ID */
+    @Excel(name = "公告序号", cellType = ColumnType.NUMERIC)
+    @Id(keyType = KeyType.Auto)
     private Long noticeId;
 
     /** 公告标题 */
+    @Excel(name = "公告标题")
+    @Xss(message = "公告标题不能包含脚本字符")
+    @NotBlank(message = "公告标题不能为空")
+    @Size(min = 0, max = 50, message = "公告标题不能超过50个字符")
     private String noticeTitle;
 
     /** 公告类型（1通知 2公告） */
+    @Excel(name = "公告类型", readConverterExp = "1=通知,2=公告")
     private String noticeType;
 
     /** 公告内容 */
     private String noticeContent;
 
     /** 公告状态（0正常 1关闭） */
+    @Excel(name = "公告状态", readConverterExp = "0=正常,1=关闭")
     private String status;
-
-    public Long getNoticeId()
-    {
-        return noticeId;
-    }
-
-    public void setNoticeId(Long noticeId)
-    {
-        this.noticeId = noticeId;
-    }
-
-    public void setNoticeTitle(String noticeTitle)
-    {
-        this.noticeTitle = noticeTitle;
-    }
-
-    @Xss(message = "公告标题不能包含脚本字符")
-    @NotBlank(message = "公告标题不能为空")
-    @Size(min = 0, max = 50, message = "公告标题不能超过50个字符")
-    public String getNoticeTitle()
-    {
-        return noticeTitle;
-    }
-
-    public void setNoticeType(String noticeType)
-    {
-        this.noticeType = noticeType;
-    }
-
-    public String getNoticeType()
-    {
-        return noticeType;
-    }
-
-    public void setNoticeContent(String noticeContent)
-    {
-        this.noticeContent = noticeContent;
-    }
-
-    public String getNoticeContent()
-    {
-        return noticeContent;
-    }
-
-    public void setStatus(String status)
-    {
-        this.status = status;
-    }
-
-    public String getStatus()
-    {
-        return status;
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("noticeId", getNoticeId())
-            .append("noticeTitle", getNoticeTitle())
-            .append("noticeType", getNoticeType())
-            .append("noticeContent", getNoticeContent())
-            .append("status", getStatus())
-            .append("createBy", getCreateBy())
-            .append("createTime", getCreateTime())
-            .append("updateBy", getUpdateBy())
-            .append("updateTime", getUpdateTime())
-            .append("remark", getRemark())
-            .toString();
-    }
 }
